b3146a99e93a94342774f4616c46030d275975a5
hodev-dev <hodev.dev@gmail.com>
2020-08-18 16:30:44 +0430

    IF component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useCallback } from 'react';
+import IF, { THEN, ELSE } from './Logics/IF';
 function App() {
+
+  const [state, setState] = useState(false);
+  const [something] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setState(true);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IF variable={state} logic={'==='} check={true}>
+        <THEN>
+          <h1>{state.toString()}</h1>
+        </THEN>
+        <ELSE>
+          <h1>{state.toString()}</h1>
+        </ELSE>
+      </IF>
+      <IF variable={something} logic={'==='} check={true}>
+        <THEN>
+          <h1>{state.toString()}</h1>
+        </THEN>
+      </IF>
+      <button onClick={handleClick}>change State</button>
     </div>
   );
 }
